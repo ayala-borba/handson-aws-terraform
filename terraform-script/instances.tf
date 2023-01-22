9138ea1b8053d28a9ead7f12c259d519b511afd1
Ayala Borba <ayala.borba@zup.com.br>
2023-01-22 13:57:06 -0300

    initial commit

---
+++ terraform-script/instances.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "instances" {
+
+  count = 3
+
+  ami = "ami-0fc61db8544a617ed"
+  instance_type = "t2.micro"
+  subnet_id = element(aws_subnet.public_subnet.*.id, count.index)
+
+  key_name = aws_key_pair.keypair.key_name
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id, aws_security_group.database.id,
+                            aws_security_group.load_balancer_tg.id, aws_security_group.allow_outbound.id]
+
+user_data = templatefile("${path.module}/user_data/install_app.tmpl", {database_endpoint = module.rds.this_db_instance_endpoint})
+
+  tags = { 
+    Name = "zup_instances"
+  }
+}
+
+resource "aws_key_pair" "keypair" {
+  public_key = file("key/zup_key.pub")
+}
+
+output "public_ips" {
+  value = join(", ", aws_instance.instances.*.public_ip)
+}
